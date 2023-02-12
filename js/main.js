f76e0f639cb0d0c5021e6724e876489a60b125f1
Tom Koreny <tom@tomkoreny.com>
2023-02-12 23:55:29 +0100

    add timeline

--- js/main.js
+++ js/main.js
@@ -25,25 +25,37 @@ function previousStory() {
 function nextTimeline() {
   const timelines = document.querySelector('ul.timeline').children;
   const arr = Array.from(timelines).filter(el => el.classList.contains('li'));
-  const activeStoryIndex = arr.findIndex(el => el.classList.contains('active'))
-  arr[activeStoryIndex].classList.remove('active');
-  if (activeStoryIndex + 1 === arr.length) {
-    arr[0].classList.add('active');
-    return
-  }
-  arr[activeStoryIndex + 1].classList.add('active');
+  const activeStoryIndex = arr.findIndex(el => !el.classList.contains('hidden'))
+  arr[activeStoryIndex].classList.add('hidden');
+  arr[activeStoryIndex + 1].classList.add('hidden');
+  arr[activeStoryIndex + 2].classList.add('hidden');
+  arr[activeStoryIndex + 3].classList.add('hidden');
+  arr[activeStoryIndex + 4].classList.add('hidden');
+  arr[activeStoryIndex + 5].classList.remove('hidden');
+  arr[activeStoryIndex + 6].classList.remove('hidden');
+  arr[activeStoryIndex + 7].classList.remove('hidden');
+  arr[activeStoryIndex + 8].classList.remove('hidden');
+  arr[activeStoryIndex + 9].classList.remove('hidden');
+  document.querySelector('a#next-timeline').classList.add('hidden');
+  document.querySelector('a#previous-timeline').classList.remove('hidden');
 }
 
 function previousTimeline() {
   const timelines = document.querySelector('ul.timeline').children;
   const arr = Array.from(timelines).filter(el => el.classList.contains('li'));
-  const activeStoryIndex = arr.findIndex(el => el.classList.contains('active'))
-  arr[activeStoryIndex].classList.remove('active');
-  if (activeStoryIndex + 1 === arr.length) {
-    arr[0].classList.add('active');
-    return
-  }
-  arr[activeStoryIndex + 1].classList.add('active');
+  const activeStoryIndex = arr.findIndex(el => !el.classList.contains('hidden'))
+  arr[activeStoryIndex].classList.add('hidden');
+  arr[activeStoryIndex + 1].classList.add('hidden');
+  arr[activeStoryIndex + 2].classList.add('hidden');
+  arr[activeStoryIndex + 3].classList.add('hidden');
+  arr[activeStoryIndex + 4].classList.add('hidden');
+  arr[activeStoryIndex - 1].classList.remove('hidden');
+  arr[activeStoryIndex - 2].classList.remove('hidden');
+  arr[activeStoryIndex - 3].classList.remove('hidden');
+  arr[activeStoryIndex - 4].classList.remove('hidden');
+  arr[activeStoryIndex - 5].classList.remove('hidden');
+  document.querySelector('a#previous-timeline').classList.add('hidden');
+  document.querySelector('a#next-timeline').classList.remove('hidden');
 }
 
 addEventListener('scroll', () => {
